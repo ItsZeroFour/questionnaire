d89b6fa5dabcf76f317c07363109a59c7ddec982
Ivan24-wq <iv8747583@gmail.com>
2024-11-30 14:48:06 +0300

    authorization

---
+++ cpp/auth.cpp/auth_telegram.cpp
@@ -0,0 +1,21 @@
+#include <openssl/hmac.h>
+#include <nlohmann/json.hpp>
+#include <string>
+#include "telegram_auth.hpp"
+using namespace std;
+
+bool verify_TelegramSignature(const nlohmann::json& data){
+    const string secretKey = "YOUR_SECRET_KEY";
+    string checkString = " ";
+
+    for(auto& [key_value] : data.items()){
+        if(key != "hash"){
+            checkString += key + "=" + value.dump() + "\n";
+        }
+    }
+
+    unsigned char* hmac_result = HMAC(EVP_sha256(), secretKey.c_str(), secretKey.size(), (unsigned char*) checkString.c_str(), checkString.size(), NULL, NULL);
+    string providedHash = data["hash"];
+    return providedHash == string((char*)hmax_result);
+
+}
