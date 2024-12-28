3abe3601c5847546a4154be3ec2efad5146d6f0e
ItsZeroFour <itsZeroFourX@gmail.com>
2024-12-28 12:18:01 +0300

    add register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Register from "./pages/register/Register";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="page">
+      <div className="container">
+        <main>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+      </div>
     </div>
   );
 }
